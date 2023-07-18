29bc7975b0ec7e725a15fa84531d0292eb8e0209
charlie Benger-stevenson <suityou01@yahoo.co.uk>
2023-07-18 16:52:52 +0100

    Some fixed for compilation errors of the ino

--- uplighter.ino
+++ uplighter.ino
@@ -1,10 +1,11 @@
 
 #include <WiFi.h>
+#include <WebServer.h>
 #include <ArduinoJson.h>
 #include <FastLED.h>
 
 #define NUM_LEDS 60
-#define DATA_PIN 6
+#define DATA_PIN 5
 #define LED_TYPE    WS2812B
 
 CRGB leds[NUM_LEDS];
@@ -15,7 +16,7 @@ char pass[] = "";
 int status = WL_IDLE_STATUS;
 int WIFI_LOGIN_ATTEMPTS = 3;
 
-WiFiServer server(80);
+WebServer server(80);
 
 void print_var(char * placeholder, char * message, char * var){
   int buffer_len = 100;
@@ -30,7 +31,7 @@ void print_var(char * placeholder, char * message, char * var){
 
 void setup_led_matrix(){
     Serial.println("Setting up LED Matrix 4x4");
-    FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS);
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 }
 
 void connect_wifi() {
@@ -61,31 +62,35 @@ void set_led_color(int led_id, int r, int g, int b){
     leds[led_id].blue = b;
 }
 
-void getBody() {
+int getBody() {
     if (server.hasArg("plain") == false) {
         
     }
     String body = server.arg("plain");
-    deserializeJson(jsonDocument, body);
+    DeserializationError error = deserializeJson(jsonDocument, body);
+    if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+    return false;
+  }
 }
 
 void postLed() {
     
     Serial.println("POST /led");
-    getBody();
-
-    Serial.println(jsonDocument['led']);
-    Serial.println(jsonDocument['r']);
-    Serial.println(jsonDocument['g']);
-    Serial.println(jsonDocument['b']);
+    if(getBody() == true) {
+      Serial.println(jsonDocument['led'].as<const char*>());
+      Serial.println(jsonDocument['r'].as<const char*>());
+      Serial.println(jsonDocument['g'].as<const char*>());
+      Serial.println(jsonDocument['b'].as<const char*>());
     
-    int led = (int)jsonDocument['led'];
-    int r = (int)jsonDocument['r'];
-    int g = (int)jsonDocument['g'];
-    int b = (int)jsonDocument['b'];
+      int led = jsonDocument['led'].as<int>();
+      int r = jsonDocument['r'].as<int>();
+      int g = jsonDocument['g'].as<int>();
+      int b = jsonDocument['b'].as<int>();
 
-    set_led_color(led, r, g, b);
-    
+      set_led_color(led, r, g, b);
+    }
 }
 
 void getLed() {
@@ -96,12 +101,14 @@ void postLeds() {
     Serial.println("POST /leds");
     getBody();
     JsonArray array = jsonDocument['leds'].as<JsonArray>();
+    /*
     for(JsonVariant led : array) {
-        Serial.println(led['led']);
-        Serial.println(led['r']);
-        Serial.println(led['g']);
-        Serial.println(led['b']);
+        Serial.println((char *)led['led']);
+        Serial.println((char *)led['r']);
+        Serial.println((char *)led['g']);
+        Serial.println((char *)led['b']);
     }
+    */
 }
 
 void getLeds() {
@@ -127,40 +134,6 @@ void setup() {
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if (client) {
-    Serial.println("new web client");
-    bool currentLineIsBlank = true;
-
-    while (client.connected()) {
-
-      if (client.available()) {
-
-        char c = client.read();
-
-        Serial.write(c);
-
-        // if you've gotten to the end of the line (received a newline
-        // character) and the line is blank, the http request has ended,
-        // so you can send a reply
-
-        if (c == '\n' && currentLineIsBlank) {
-
-          // send a standard http response header
-
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-        
-        }
-      }
-    }
-    client.stop();
-    Serial.println("client disconnected");
-  }
+  server.handleClient();
   delay(10); // this speeds up the simulation
 }
